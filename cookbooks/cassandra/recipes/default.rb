dc1102774732dee558095bdb73e70b13fd13cdd3
Lyric Hartley <lyric@lyric-hartleys-macbook-pro-2.local>
2009-08-17 13:13:58 -0700

    a bit more done to cassandra recipe, going to test and tweak

--- cookbooks/cassandra/recipes/default.rb
+++ cookbooks/cassandra/recipes/default.rb
@@ -7,17 +7,24 @@ package "sun-java6-jre" do
   action :install
 end
 
-directory "/mnt/cassandra" do
+directory "#{node[:basedir]}" do
   owner node[:owner_name]
   group node[:owner_name]
   mode 0755
   recursive true
 end
 
-remote_file "/tmp/cassandra.tgz" do
+remote_file "/opt/cassandra.tgz" do
   source "http://apache.mirror.facebook.net/incubator/cassandra/0.3.0/apache-cassandra-incubating-0.3.0-bin.tar.gz"
   mode "0644"
   checksum "bfbb9cd29866ac31217f72c57e277a67" # A SHA256 (or portion thereof) of the file.
+end
+
+execute "untar" do
+  cwd "/opt"
+  command "tar -zxf cassandra.tgz"
+  creates "/opt/cassandra"  
+  action :run
 end
 
 template "/etc/cassandra/storage-conf.xml" do
@@ -26,6 +33,22 @@ template "/etc/cassandra/storage-conf.xml" do
   mode 0644
   source "storage-conf.xml.erb"
   variables({
-    :basedir => '/mnt/cassandra',
+    :node => node
   })
 end
+
+template "/etc/cassandra/log4j.properties" do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source "log4j.properties.erb"
+  variables({
+    :node => node
+  })
+end
+
+link "/etc/cassandra/" do
+  to "/opt/cassandra/"
+end
+
+
